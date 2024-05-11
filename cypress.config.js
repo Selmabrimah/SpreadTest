6b161c9c899ab5783627310f149a49ce556e2964
Florent Houetchekpo <serlybreezybrimah10@gmail.com>
2024-05-11 13:58:45 +0200

    added stage and demo environmental data files and modified script in package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require("cypress");
+// promisified fs module
+const fs = require('fs-extra')
+const path = require('path')
 
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress/config', `${file}.json`)
+  if(!fs.existsSync(pathToConfigFile)){
+    console.log('There is no custom file found')
+    return {}
+  }
+
+  return fs.readJson(pathToConfigFile)
+}
 module.exports = defineConfig({
   viewportHeight:1400,
   viewportWidth:1600,
@@ -12,6 +24,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const file = config.env.configFile || ''
+      return getConfigurationByFile(file)
     },
     baseUrl:'https://opensource-demo.orangehrmlive.com'
   },
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe('Login Feature', () => {
     it('Verify User Can Login with right Credentials', () => {
-      cy.visit('https://opensource-demo.orangehrmlive.com')
+      cy.visit('/')
       cy.get('[src="/web/images/ohrm_branding.png?v=1711595107870"]').should('be.visible')
       cy.get('[placeholder="Username"]').type('Admin')
       cy.get('[placeholder="Password"]').type('admin123')
