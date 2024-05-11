72e92b50b5598c2dc91270c3cfaebb3725c16879
Florent Houetchekpo <serlybreezybrimah10@gmail.com>
2024-05-11 16:08:56 +0200

    added necessary manual trigger

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 describe('Login Feature', () => {
-    it('Verify User Can Login with right Credentials', () => {
+    it('Verify User Can Login with right Credentials (Fast approach)', () => {
       cy.visit('/')
       cy.get('[src="/web/images/ohrm_branding.png?v=1711595107870"]').should('be.visible')
       cy.get('[placeholder="Username"]').type('Admin')
@@ -8,7 +8,7 @@ describe('Login Feature', () => {
       cy.url().should('include', 'dashboard');
     })
 
-    it('Verify User Cannot Login with wrong Credentials', () => {
+    it('Verify User Cannot Login with wrong Credentials (Fast approach)', () => {
       cy.visit('/')
       cy.get('[src="/web/images/ohrm_branding.png?v=1711595107870"]').should('be.visible')
       cy.get('[placeholder="Username"]').type('Admin123')
--- cypress/e2e/loginWithCypressCommand.cy.js
+++ cypress/e2e/loginWithCypressCommand.cy.js
@@ -4,12 +4,12 @@ const loginPage = new LoginPage();
 describe('Login Feature With the use of cypress Command', () => {
 
 
-    it('Verify User Can Login with right Credentials', () => {
+    it('Verify User Can Login with right Credentials (pom and command approach)', () => {
        cy.login(Cypress.env('USERNAME'),Cypress.env('PASSWORD'))
         loginPage.verifyLoginSuccess();
     });
 
-    it('Verify User Cannot Login with wrong Credentials', () => {
+    it('Verify User Cannot Login with wrong Credentials (pom and command approach)', () => {
         const username= 'Admin';
         const password= 'admin';
         cy.login(username,password)
